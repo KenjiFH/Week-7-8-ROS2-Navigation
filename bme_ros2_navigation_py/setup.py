97328316db937a04009bd352df0cec1eb53eff75
David Dudas <david.dudas@outlook.com>
2024-11-13 10:07:28 +0100

    add a python package that we will use for navigation

---
+++ bme_ros2_navigation_py/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'bme_ros2_navigation_py'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='david',
+    maintainer_email='david@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'map_republisher = bme_ros2_navigation_py.map_republisher:main',
+            'send_initialpose = bme_ros2_navigation_py.send_initialpose:main',
+        ],
+    },
+)
